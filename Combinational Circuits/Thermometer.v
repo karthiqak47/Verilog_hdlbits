c46236dff973a71844d31d85c8aabc25b7354868
Karthik A K <167605224+karthiqak47@users.noreply.github.com>
2025-09-15 22:10:03 +0530

    Create Thermometer.v

---
+++ Combinational Circuits/Thermometer.v
@@ -0,0 +1,15 @@
+module top_module (
+    input too_cold,
+    input too_hot,
+    input mode,
+    input fan_on,
+    output heater,
+    output aircon,
+    output fan
+); 
+    assign heater=mode&&too_cold;
+    assign aircon=too_hot&&~mode;
+    assign fan=heater|fan_on|ac ;
+    
+
+endmodule
